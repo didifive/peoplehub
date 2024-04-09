7242fb47c33dc42bca832a630fb4f5389969eb72
Luis Zancanela <luisczdidi@gmail.com>
2024-04-08 22:25:55 -0300

    init: init project

---
+++ src/main/java/it/zancanela/peoplehub/PeopleHubApplication.java
@@ -0,0 +1,13 @@
+package it.zancanela.peoplehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/zancanela/peoplehub/PeopleHubApplicationTests.java
@@ -0,0 +1,13 @@
+package it.zancanela.peoplehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

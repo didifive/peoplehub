136169f2361a2d4c6db7bc346490c455e69aba3d
Luis Zancanela <luisczdidi@gmail.com>
2024-04-10 00:00:34 -0300

    feat: add swagger config

--- src/main/java/it/zancanela/peoplehub/PeopleHubApplication.java
+++ src/main/java/it/zancanela/peoplehub/PeopleHubApplication.java
@@ -1,9 +1,12 @@
 package it.zancanela.peoplehub;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class PeopleHubApplication {
 
 	public static void main(String[] args) {
